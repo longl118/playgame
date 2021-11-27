688c451129e4b8aa97414032f283f8941aba387e
longl118 <1101576544@qq.com>
2021-11-28 01:49:07 +0800

    first commit

---
+++ playgame/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace playgame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.ImageLocation = "D:\\VSdaima\\playgame\\tu\\";
+            this.pictureBox1.Location = new System.Drawing.Point(12, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(351, 581);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(383, 16);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(735, 334);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(383, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "进入游戏";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(562, 378);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "玩家输入";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(632, 363);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 34);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(489, 443);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 33);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "前";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(383, 498);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 30);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "左";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(580, 498);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 30);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "右";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(489, 545);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(81, 31);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "后";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(759, 430);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "快捷命令";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(841, 420);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(207, 35);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(398, 420);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "行走动作";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(972, 363);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(76, 34);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "确定";
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(967, 516);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(81, 37);
+            this.button9.TabIndex = 16;
+            this.button9.Text = "退出游戏";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 609);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ playgame/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace playgame
+{
+    public partial class Form1 : Form
+    {
+        private NetworkStream stream;
+        private TcpClient tcpClient = new TcpClient();
+        private int picture;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+            private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //向指定的IP地址的服务器发出连接请求
+                tcpClient.Connect("10.1.230.74", 3900);
+                listBox1.Items.Add("连接成功！");
+                stream = tcpClient.GetStream();
+                byte[] data = new byte[1024];
+                //判断网络流是否可读            
+                if (stream.CanRead)
+                {
+                    int len = stream.Read(data, 0, data.Length);
+                    string msg = Encoding.Default.GetString(data, 0, data.Length);
+                    string str = "\r\n";
+                    char[] str1 = str.ToCharArray();
+                    string[] msg1 = msg.Split(str1);
+                    for (int j = 0; j < msg1.Length; j++)
+                    {
+                        listBox1.Items.Add(msg1[j]);
+                         music_play();
+                    }
+                }
+            }
+            catch
+            {
+                listBox1.Items.Add("连接失败！");
+            }
+        }
+
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            music_play();
+            if (tcpClient.Connected)
+            {
+                //向服务器发送数据
+                string msg = textBox1.Text;
+                Byte[] outbytes = System.Text.Encoding.Default.GetBytes(msg + "\n");
+                stream.Write(outbytes, 0, outbytes.Length);
+                byte[] data = new byte[1024];
+                //接收服务器回复数据
+                if (stream.CanRead)
+                {
+                    int len = stream.Read(data, 0, data.Length);
+                    string msg1 = Encoding.Default.GetString(data, 0, data.Length);
+                    string str = "\r\n";
+                    char[] str1 = str.ToCharArray();
+                    string[] msg2 = msg1.Split(str1);
+                    for (int j = 0; j < msg2.Length; j++)
+                    {
+                        listBox1.Items.Add(msg2[j]);
+                    }
+                }
+            }
+            else
+            {
+                listBox1.Items.Add("连接已断开");
+            }
+            textBox1.Clear();
+        }
+
+        private void music_play()
+        {
+            SoundPlayer sp = new SoundPlayer();
+            sp.SoundLocation = @"D:\VSdaima\playgame\费玉清 - 夏之旅.wav";       //你的音乐文件名称，且注意必须是wav文件
+            sp.PlayLooping();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Thread th = new Thread(pic_play);
+            th.IsBackground = true;
+            th.Start();
+        }
+
+        void pic_play()
+        {
+            picture++;   //记得在前面定义变量picture
+            string picturePath = @"D:\VSdaima\playgame\tu\" + picture + ".jpeg";
+            //设置图片填充
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Image = Image.FromFile(picturePath);
+            if (picture == 5)
+                picture = 0;
+        }
+
+    }
+}
